bbfbc285bdd135cebc1924c1d67cb47e860dc997
Dmitriy N. Borisov <borik.internet@gmail.com>
2020-05-20 01:53:00 +0300

    LICENSE fixes

--- event_listener.go
+++ event_listener.go
@@ -1,5 +1,5 @@
 /*
-Copyright (c) 2019 Dmitrii Borisov <dborisov@mail.ru>
+Copyright (c) 2019-2020 Dmitrii Borisov <dborisov@mail.ru>
 
 Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated
 documentation files (the "Software"), to deal in the Software without restriction, including without limitation the
@@ -15,6 +15,8 @@ COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER I
 OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 */
 
+// fsEventListener handle FreeSWITCH events with provided functions
+// See test/event_listener_test.go for usage examples
 package fsEventListener
 
 import (
